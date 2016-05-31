b3964bd8ef213acb528d0bc157e11157318f8677
antbadija <antonioleoncarrillo@gmail.com>
2016-05-31 12:52:14 +0200

    Added new models and requirements

--- eathubApp/webapp/admin.py
+++ eathubApp/webapp/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Tastes
+from .models import Tastes, Comment, Step, Following
 
 admin.site.register(Tastes)
+admin.site.register(Comment)
+admin.site.register(Step)
+admin.site.register(Following)
--- eathubApp/webapp/models.py
+++ eathubApp/webapp/models.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import ForeignKey
+
 
 # Create your models here.
 
 
 # Validators
+
 
 def validate_savour(savour):
     if savour <= -1 or savour >= 100:
@@ -18,3 +22,39 @@ class Tastes(models.Model):
    bitter = models.IntegerField(validators=[validate_savour])
    sweet = models.IntegerField(validators=[validate_savour])
    spicy = models.IntegerField(validators=[validate_savour])
+
+   def __str__(self):
+        return "{}, {}, {}, {}, {}".format(self.salty, self.sour, self.bitter,
+                                           self.sweet, self.spicy)
+
+
+class Comment(models.Model):
+    text = models.TextField(blank=False)
+    create_date = models.BigIntegerField()
+    user_own = ForeignKey(User,related_name='user', default='', null=False)
+    is_banned = models.BooleanField(default=False)
+    #id_recipe = models.ForeignKey(Recipe, related_name='comments', default='')
+
+    def __str__(self):
+        return self.text
+
+
+class Step(models.Model):
+    text = models.TextField(blank=False)
+    image = models.ImageField(upload_to="images/recipe/", null=True)
+    #recipe = models.ForeignKey(Recipe, related_name='recipes',default='')
+
+
+
+class Picture(models.Model):
+    image = models.ImageField(upload_to="images/", null=False)
+
+
+class Following(models.Model):
+    display_name = models.CharField(max_length=50, blank=False)
+    username = models.CharField(max_length=50, blank=False)
+    user = models.ForeignKey(User)
+
+    @staticmethod
+    def create_following(user):
+        return Following(display_name=user.profile.get().display_name, username=user.username, user=user)
